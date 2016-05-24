f4dd93a6a62faa3f77c43216350e3a733ae85fca
Luis De La Espriella <luisdelae@gmail.com>
2016-05-24 14:55:06 -0500

    first commit. has data and hard mode app. need to figure out how to delete salary maybe using nested lists.

---
+++ CSChallengeWknd1/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace CSChallengeWknd1
+{
+    partial class frmEmployees
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblCostOfSalaryMonthly = new System.Windows.Forms.Label();
+            this.lstEmployeeData = new System.Windows.Forms.ListBox();
+            this.btnDelEmployee = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(12, 42);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 0;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(118, 42);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 1;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(224, 42);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 2;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(330, 42);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(436, 42);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(100, 20);
+            this.txtSalary.TabIndex = 4;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(542, 39);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(87, 23);
+            this.btnSubmit.TabIndex = 5;
+            this.btnSubmit.Text = "Add Employee";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Emp ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(327, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Title";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(433, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Salary";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(262, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Monthly Salary Cost: ";
+            // 
+            // lblCostOfSalaryMonthly
+            // 
+            this.lblCostOfSalaryMonthly.AutoSize = true;
+            this.lblCostOfSalaryMonthly.Location = new System.Drawing.Point(361, 99);
+            this.lblCostOfSalaryMonthly.Name = "lblCostOfSalaryMonthly";
+            this.lblCostOfSalaryMonthly.Size = new System.Drawing.Size(19, 13);
+            this.lblCostOfSalaryMonthly.TabIndex = 12;
+            this.lblCostOfSalaryMonthly.Text = "$0";
+            // 
+            // lstEmployeeData
+            // 
+            this.lstEmployeeData.FormattingEnabled = true;
+            this.lstEmployeeData.Location = new System.Drawing.Point(52, 132);
+            this.lstEmployeeData.Name = "lstEmployeeData";
+            this.lstEmployeeData.Size = new System.Drawing.Size(482, 134);
+            this.lstEmployeeData.TabIndex = 13;
+            // 
+            // btnDelEmployee
+            // 
+            this.btnDelEmployee.Location = new System.Drawing.Point(540, 185);
+            this.btnDelEmployee.Name = "btnDelEmployee";
+            this.btnDelEmployee.Size = new System.Drawing.Size(89, 23);
+            this.btnDelEmployee.TabIndex = 14;
+            this.btnDelEmployee.Text = "Delete Emp";
+            this.btnDelEmployee.UseVisualStyleBackColor = true;
+            this.btnDelEmployee.Click += new System.EventHandler(this.btnDelEmployee_Click);
+            // 
+            // frmEmployees
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 288);
+            this.Controls.Add(this.btnDelEmployee);
+            this.Controls.Add(this.lstEmployeeData);
+            this.Controls.Add(this.lblCostOfSalaryMonthly);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtId);
+            this.Name = "frmEmployees";
+            this.Text = "Employees Cost";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblCostOfSalaryMonthly;
+        private System.Windows.Forms.ListBox lstEmployeeData;
+        private System.Windows.Forms.Button btnDelEmployee;
+    }
+}
---
+++ CSChallengeWknd1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSChallengeWknd1
+{
+    public partial class frmEmployees : Form
+    {
+        double empSalaryCostMonthly = 0;
+        //List<List<string>> empDataList = new List<List<string>>();
+
+        List<string> empDataList = new List<string>();
+
+        public frmEmployees()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            empSalaryCostMonthly += Convert.ToDouble(txtSalary.Text);
+            lblCostOfSalaryMonthly.Text = "$" + Convert.ToString(empSalaryCostMonthly);
+
+            //Might need to add an array instead of strings to be able to delete the list.selectedindex 
+            //at the selected item, and delete index at [4] of this selected item for it to
+            //subtract the salary from the total once the employee is deleted
+            empDataList.Add(txtId.Text + "\t\t" + txtFirstName.Text + "\t\t" + txtLastName.Text + "\t\t" + txtTitle.Text + "\t\t" + txtSalary.Text + "\n");
+            //List<string> empInfo = new List<string> { Convert.ToString(txtId), Convert.ToString(txtFirstName), Convert.ToString(txtLastName)
+            //    , Convert.ToString(txtTitle), Convert.ToString(txtSalary) };
+            //empDataList.Add(empInfo);
+            lstEmployeeData.DataSource = empDataList.ToList();
+        }
+
+        private void btnDelEmployee_Click(object sender, EventArgs e)
+        { 
+            empDataList.RemoveAt(lstEmployeeData.SelectedIndex);
+
+            //empSalaryCostMonthly -= Convert.ToDouble(empDataList[lstEmployeeData.SelectedIndex]);
+            //lblCostOfSalaryMonthly.Text = Convert.ToString(empSalaryCostMonthly);
+
+            lstEmployeeData.DataSource = null;
+            lstEmployeeData.DataSource = empDataList.ToArray();
+        }
+    }
+}
